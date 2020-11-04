1840deaf3e36b155610697b04d2285efa5588923
Bilal Babale <bilalbabale@gmail.com>
2020-11-04 09:45:11 +0100

    initial commit

---
+++ Supervised.py
@@ -0,0 +1,29 @@
+import numpy as np
+import scipy.optimize as op
+
+class LinearReg():
+
+    def __init__ (self):
+        pass
+
+
+    def train(self, X,y):
+        pass
+
+
+    def predict(self, X,y):
+        pass
+
+
+class LogisticReg():
+
+    def __init__ (self):
+        pass
+
+
+    def train(self, X,y):
+        pass
+
+
+    def predict(self, X,y):
+        pass
