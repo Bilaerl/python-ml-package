70b660871a8bd1bc6f86c924991759fc32f61859
Bilal Babale <bilalbabale@gmail.com>
2020-11-06 19:23:10 +0100

    feat: Add FeatureMapper class

--- Preprocessing.py
+++ Preprocessing.py
@@ -6,3 +6,24 @@ def add_bias(X):
     X = np.hstack([bias,X]) # add bias to data
 
     return X
+
+
+class FeatureMapper:
+
+    def __init__(self, degree):
+        self.degree = degree
+    
+
+    def __repr__(self):
+        return f"FeatureMapper(degree={self.degree})"
+    
+
+    def map(self, X1,X2):
+        out = np.ones((len(X1),1))
+        for i in range(1,self.degree+1):
+            for j in range(i+1):
+                temp = (X1**(i-j)) * (X2**j)
+                temp = temp.reshape(len(temp),1)
+                out = np.hstack((out,temp))
+        return out
+        
